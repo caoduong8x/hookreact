288868143c11de88d86f97cdc6dd79cd8bbc40b1
caoduong8x <caoduong.it8x@gmail.com>
2023-06-14 09:43:05 +0700

    useState Hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+//console.log(React);
+
+const orders = [100, 200, 300];
 
 function App() {
+  const [counter, setCounter] = useState(() => {
+    const total = orders.reduce((total, i) => total + i);
+    return total;
+  });
+  const [info, setInfo] = useState({ name: "abc", age: "25" });
+  const handleClick = () => {
+    setCounter((a) => a + 1);
+    setCounter((a) => a + 1);
+    // setCounter(counter + 1);
+  };
+  const handleAddInfo = () => {
+    setInfo((pre) => ({ ...pre, bio: "Love pink" }));
+    // setInfo({ ...info, bio: "Love pink" });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ padding: 10 }}>
+      <h1>{counter}</h1>
+      <button onClick={handleClick}>Increase</button>
+      <h1>{JSON.stringify(info)}</h1>
+      <button onClick={handleAddInfo}>Add Proper</button>
     </div>
   );
 }
