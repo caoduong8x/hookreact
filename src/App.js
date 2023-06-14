091ca97a9e1df6521d1e5fad58502e059b252875
caoduong8x <caoduong.it8x@gmail.com>
2023-06-14 14:37:50 +0700

    Toastify added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 //console.log(React);
 
 const orders = [100, 200, 300];
@@ -15,16 +17,29 @@ function App() {
     // setCounter(counter + 1);
   };
   const handleAddInfo = () => {
-    setInfo((pre) => ({ ...pre, bio: "Love pink" }));
+    setInfo((pre) => ({ ...pre, bio: "Love food" }));
+    toast.success("🦄 Wow so easy!", {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
     // setInfo({ ...info, bio: "Love pink" });
   };
   return (
-    <div className="App" style={{ padding: 10 }}>
-      <h1>{counter}</h1>
-      <button onClick={handleClick}>Increase</button>
-      <h1>{JSON.stringify(info)}</h1>
-      <button onClick={handleAddInfo}>Add Proper</button>
-    </div>
+    <>
+      <div className="App" style={{ padding: 10 }}>
+        <h1>{counter}</h1>
+        <button onClick={handleClick}>Increase</button>
+        <h1>{JSON.stringify(info)}</h1>
+        <button onClick={handleAddInfo}>Add Proper</button>
+      </div>
+      <ToastContainer />
+    </>
   );
 }
 
