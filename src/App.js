92671e5ea17b51c239d4b698878546162ba56ddb
caoduong8x <caoduong.it8x@gmail.com>
2023-06-14 11:08:49 +0700

    Two-way binding

--- src/App.js
+++ src/App.js
@@ -1,30 +1,67 @@
 import React, { useState } from "react";
 //console.log(React);
-
-const orders = [100, 200, 300];
-
+const gifts = ["Cpu i9", "Memory 32GB RBG", "SSD 1TB"];
+const courses = [
+  { id: 1, name: "HTML, CSS" },
+  { id: 2, name: "JavaScript" },
+  { id: 3, name: "React" },
+];
 function App() {
-  const [counter, setCounter] = useState(() => {
-    const total = orders.reduce((total, i) => total + i);
-    return total;
-  });
-  const [info, setInfo] = useState({ name: "abc", age: "25" });
-  const handleClick = () => {
-    setCounter((a) => a + 1);
-    setCounter((a) => a + 1);
-    // setCounter(counter + 1);
+  const [gift, setGift] = useState();
+  const [radioChecked, setRadioChecked] = useState();
+  const [checkBoxChecked, setCheckBoxChecked] = useState([]);
+  const handleLayThuong = () => {
+    setGift(gifts[Math.floor(Math.random() * gifts.length)]);
   };
-  const handleAddInfo = () => {
-    setInfo((pre) => ({ ...pre, bio: "Love pink" }));
-    // setInfo({ ...info, bio: "Love pink" });
+  const checkBoxChange = (id) => {
+    setCheckBoxChecked((prev) => {
+      const checked = checkBoxChecked.includes(id);
+      if (checked) {
+        return checkBoxChecked.filter((i) => i !== id);
+      } else {
+        return [...prev, id];
+      }
+    });
   };
+
+  const handleSubmit = () => {};
   return (
-    <div className="App" style={{ padding: 10 }}>
-      <h1>{counter}</h1>
-      <button onClick={handleClick}>Increase</button>
-      <h1>{JSON.stringify(info)}</h1>
-      <button onClick={handleAddInfo}>Add Proper</button>
-    </div>
+    <>
+      <div className="App" style={{ textAlign: "center" }}>
+        <h1>{gift || "Chưa có phần thưởng!"}</h1>
+        <button onClick={handleLayThuong}>Lấy thưởng</button>
+      </div>
+
+      <br />
+      <div style={{ textAlign: "center" }}>
+        {courses.map((course) => (
+          <div key={course.id}>
+            <input
+              type="radio"
+              checked={radioChecked === course.id}
+              onChange={() => setRadioChecked(course.id)}
+            />
+            {course.name}
+          </div>
+        ))}
+        <button onClick={handleSubmit}>Register</button>
+      </div>
+
+      <br />
+      <div style={{ textAlign: "center" }}>
+        {courses.map((course) => (
+          <div key={course.id}>
+            <input
+              type="checkbox"
+              checked={checkBoxChecked.includes(course.id)}
+              onChange={() => checkBoxChange(course.id)}
+            />
+            {course.name}
+          </div>
+        ))}
+        <button onClick={handleSubmit}>Register</button>
+      </div>
+    </>
   );
 }
 
