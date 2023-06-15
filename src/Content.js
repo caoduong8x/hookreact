b63d4fb8c405005a8e11606ad13f78dc7a48c4c1
caoduong8x <caoduong.it8x@gmail.com>
2023-06-15 10:51:28 +0700

    useEffect 4 with timer function

--- src/Content.js
+++ src/Content.js
@@ -12,95 +12,20 @@
 //-------------------------------------------------
 
 import React, { useState, useEffect } from "react";
-const tabs = ["posts", "comments", "albums", "photos", "todos", "users"];
 
 function Content() {
-  const [title, setTitle] = useState("");
-  const [posts, setPosts] = useState([]);
-  const [type, setType] = useState("posts");
-  const [showGoToTop, setShowGoToTop] = useState(false);
-  const [width, setWidth] = useState(window.innerWidth);
-
+  const [countDown, setCountDown] = useState(180);
   useEffect(() => {
-    document.title = title;
-  });
-
-  useEffect(() => {
-    // document.title = title;
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((r) => r.json())
-      .then((p) => {
-        setPosts(p);
-      });
+    const timerID = setInterval(() => setCountDown((pre) => pre - 1), 1000);
+    return () => clearInterval(timerID);
   }, []);
 
-  useEffect(() => {
-    // document.title = title;
-    fetch(`https://jsonplaceholder.typicode.com/${type}`)
-      .then((r) => r.json())
-      .then((p) => {
-        setPosts(p);
-      });
-  }, [type]);
+  // hoac dung
+  // useEffect(() => {
+  //   const timerID = setTimeout(() => setCountDown(countDown - 1), 1000);
+  //   return () => clearTimeout(timerID);
+  // }, [countDown]);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      console.log(window.scrollY);
-      setShowGoToTop(window.scrollY >= 200);
-    };
-    window.addEventListener("scroll", handleScroll);
-
-    //cleanup function
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setWidth(window.innerWidth);
-    };
-    window.addEventListener("resize", handleResize);
-
-    //cleanup function
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-  return (
-    <>
-      {tabs.map((tab) => (
-        <button
-          key={tab}
-          stype={type === tab ? { color: "#fff", background: "#333" } : {}}
-          onClick={() => {
-            setType(tab);
-          }}>
-          {tab}
-        </button>
-      ))}
-      <input
-        type="text"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <h1>Width: {width}</h1>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>{post.title || post.name}</li>
-        ))}
-      </ul>
-      {showGoToTop && (
-        <button
-          style={{ position: "fixed", right: 20, bottom: 20 }}
-          onClick={() => {
-            document.body.scrollTop = 0;
-            document.documentElement.scrollTop = 0;
-          }}>
-          Go to top
-        </button>
-      )}
-    </>
-  );
+  return <h1>{countDown}</h1>;
 }
 export default Content;
