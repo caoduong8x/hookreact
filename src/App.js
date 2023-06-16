970d9eb7d96c3ad98470e8856e82231fd7130417
caoduong8x <caoduong.it8x@gmail.com>
2023-06-16 09:27:13 +0700

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-//useCallback():
-// - Reference type
-// - React memo()
-// ==> dùng memo() trong component con thì mới dùng useCallback trong component cha
-import React, { useCallback, useState } from "react";
+//useMemo dùng để tránh lặp lại code không cần thiết nhiều lần
+import React, { useCallback, useMemo, useState, useRef } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Content from "./Content";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [products, setProducts] = useState([]);
 
-  const handleIncrease = useCallback(() => {
-    setCount((prev) => prev + 1);
-  }, []);
-
+  const nameRef = useRef();
+  const handleSubmit = () => {
+    setProducts([
+      ...products,
+      {
+        name,
+        price: +price,
+      },
+    ]);
+    setName("");
+    setPrice("");
+    nameRef.current.focus();
+  };
+  const total = useMemo(() => {
+    const result = products.reduce((total, product) => {
+      console.log("tinh toan lai");
+      return total + product.price;
+    }, 0);
+    return result;
+  }, [products]);
   return (
     <>
       <div style={{ padding: "10px 30px" }}>
-        <Content onIncrease={handleIncrease} />
-        <h1>{count}</h1>
+        <input
+          value={name}
+          ref={nameRef}
+          placeholder="Enter name"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <br />
+        <input
+          value={price}
+          placeholder="Enter price"
+          onChange={(e) => setPrice(e.target.value)}
+        />{" "}
+        <br />
+        <button onClick={handleSubmit}>Add</button>
+        <br />
+        Total: {total}
+        <ul>
+          {products.map((product, index) => (
+            <li key={index}>
+              {product.name} - {product.price}
+            </li>
+          ))}
+        </ul>
       </div>
       <ToastContainer />
     </>
