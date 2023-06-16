445c00ca0e5e5392d17c4f6e70c45148cf56feb1
caoduong8x <caoduong.it8x@gmail.com>
2023-06-16 10:38:21 +0700

    useReducer-1

--- src/App.js
+++ src/App.js
@@ -1,60 +1,51 @@
-//useMemo dùng để tránh lặp lại code không cần thiết nhiều lần
-import React, { useCallback, useMemo, useState, useRef } from "react";
+// useState
+//1. init state
+//2. action: up/down
+//----------------------------------------------------------------
+// useReduce
+//1. init state
+//2. action: up/down
+//3. reducer
+//4. dispatch
+import React, {
+  useCallback,
+  useMemo,
+  useState,
+  useRef,
+  useReducer,
+} from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Content from "./Content";
 
-function App() {
-  const [name, setName] = useState("");
-  const [price, setPrice] = useState("");
-  const [products, setProducts] = useState([]);
+//init state
+const initState = 0;
 
-  const nameRef = useRef();
-  const handleSubmit = () => {
-    setProducts([
-      ...products,
-      {
-        name,
-        price: +price,
-      },
-    ]);
-    setName("");
-    setPrice("");
-    nameRef.current.focus();
-  };
-  const total = useMemo(() => {
-    const result = products.reduce((total, product) => {
-      console.log("tinh toan lai");
-      return total + product.price;
-    }, 0);
-    return result;
-  }, [products]);
+//action
+const UP_ACTION = "up";
+const DOWN_ACTION = "down";
+
+const reducer = (state, action) => {
+  console.log("reducer running");
+  switch (action) {
+    case UP_ACTION:
+      return state + 1;
+    case DOWN_ACTION:
+      return state - 1;
+    default:
+      throw new Error("Action is not valid");
+  }
+};
+
+function App() {
+  const [count, dispatch] = useReducer(reducer, initState);
+
   return (
     <>
       <div style={{ padding: "10px 30px" }}>
-        <input
-          value={name}
-          ref={nameRef}
-          placeholder="Enter name"
-          onChange={(e) => setName(e.target.value)}
-        />
-        <br />
-        <input
-          value={price}
-          placeholder="Enter price"
-          onChange={(e) => setPrice(e.target.value)}
-        />{" "}
-        <br />
-        <button onClick={handleSubmit}>Add</button>
-        <br />
-        Total: {total}
-        <ul>
-          {products.map((product, index) => (
-            <li key={index}>
-              {product.name} - {product.price}
-            </li>
-          ))}
-        </ul>
+        <h1>{count}</h1>
+        <button onClick={() => dispatch(DOWN_ACTION)}>Down</button>
+        <button onClick={() => dispatch(UP_ACTION)}>Up</button>
       </div>
       <ToastContainer />
     </>
